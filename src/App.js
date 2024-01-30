0c10b87f7d420a0becb3d3c5eae2bf9ed0beaa25
kushal <kushal2004>
2024-01-31 00:07:38 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/app/api/apiSlice.js 	new file:   src/app/store.js 	new file:   src/features/notes/Note.js 	modified:   src/features/notes/NotesList.js 	new file:   src/features/notes/notesApiSlice.js 	new file:   src/features/users/User.js 	modified:   src/features/users/UsersList.js 	new file:   src/features/users/usersApiSlice.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -11,23 +11,20 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-        <Route path="login" element={<Login />} />
+            <Route index element={<Public />} />
+            <Route path="login" element={<Login />} />
+            <Route path="dash" element={<DashLayout />}>
 
-        <Route path="dash" element={<DashLayout />}>
+                <Route index element={<Welcome />} />
+                <Route path="notes">
+                    <Route index element={<NotesList />} />
+                </Route>
 
-          <Route index element={<Welcome />} />
+                <Route path="users">
+                    <Route index element={<UsersList />} />
+                </Route>
 
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
-
-          <Route path="users">
-            <Route index element={<UsersList />} />
-          </Route>
-
-        </Route>{/* End Dash */}
-
+            </Route>{/* End Dash */}
       </Route>
     </Routes>
   );
